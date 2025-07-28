726588b892221c5694d6cd84ad36e49a10a83c4e
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-28 13:02:31 +0500

    ..

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,19 +1,18 @@
-// /app/about/[id]/page.tsx
 
 import { authors } from "@/data/authors";
 import { notFound } from "next/navigation";
 
-type Pageprops = {
+type PageProps = {
   params: {
     id: string;
   };
 };
 
-export default function AuthorPage({ params }: Pageprops) {
+export default function AuthorPage({ params }: PageProps) {
   const author = authors.find((author) => author.id === params.id);
 
   if (!author) {
-    return notFound();
+    notFound();
   }
 
   return (
