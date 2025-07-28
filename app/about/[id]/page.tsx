702d7744344812a2ed8a17294cef433b72ccb7a5
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-28 12:37:35 +0500

    push

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,19 +1,25 @@
+// /app/about/[id]/page.tsx
+
 import { authors } from "@/data/authors";
 import { notFound } from "next/navigation";
-type props = {
-    params : {
-        id: string;
-    }
-}
-export default function AuthorPage({params} : props) {
-    const author = authors.find(author => author.id === params.id);
-    if (!author) {
-        return notFound();
-    }
-    return (
-        <div>
-            <h1>{author.name}</h1>
-            <p>{author.bio}</p>
-        </div>
-    );
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function AuthorPage({ params }: Props) {
+  const author = authors.find((author) => author.id === params.id);
+
+  if (!author) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <h1>{author.name}</h1>
+      <p>{author.bio}</p>
+    </div>
+  );
 }
