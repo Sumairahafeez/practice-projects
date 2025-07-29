5aafe2feccead5bcf1dd7134ea357cba2e3a2ca9
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-29 18:14:35 +0500

    build ready

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 import Card from "@/component/card";
 import React from 'react';
+
+interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+}
+
 export default function StorePage() {
     // const products = [
     //     { id: 1, title: "Product 1", description: "This is a great product.", price: 19.99 },
     //     { id: 2, title: "Product 2", description: "This product is even better.", price: 29.99 },
     //     { id: 3, title: "Product 3", description: "You will love this product.", price: 39.99 },
     // ];
-    const [products, setProducts] = React.useState([]);
+    const [products, setProducts] = React.useState<Product[]>([]);
     const fetchProducts = async () => {
         const response = await fetch('https://dummyjson.com/products');
         const data = await response.json();
@@ -25,7 +33,7 @@ export default function StorePage() {
                 {/* {products.map(product => (
                     <Card key={product.id} title={product.title} description={product.description} price={product.price} />
                 ))} */}
-                {products.map((product: any) => (
+                {products.map((product: Product) => (
                     <Card key={product.id} title={product.title} description={product.description} price={product.price} />
                 ))}
             </div>
