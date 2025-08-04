c4fd9857f00e27c6c7004a4e672e84d4c354de09
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-08-04 10:08:47 +0500

    some inclusions

---
+++ app/weatherApp/page.tsx
@@ -0,0 +1,10 @@
+export default function weatherApp() {
+  return (
+    <div className='flex flex-col items-center justify-center p-8 pb-8 sm:p-20'>
+      <h1>Weather App</h1>
+      <p>This is a simple weather application that provides current weather information.</p>
+      <p>Use the search functionality to find weather details for any location.</p>
+      <p>Stay updated with the latest weather conditions and forecasts.</p>
+    </div>
+  );
+}   
